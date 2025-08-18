a700c79ff85a5ab7ab8df82382ac47678d25295f
Kartik245206 <bhadana34190@gmail.com>
2025-08-18 12:36:56 +0530

    fix: remove rate limiter to resolve too many requests error

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const Database = require('./api/database');
 const connectDB = require('./config/db');
 const helmet = require('helmet');
-const rateLimit = require('express-rate-limit');
 const cookieParser = require('cookie-parser');
 const fs = require('fs');
 const multer = require('multer');
