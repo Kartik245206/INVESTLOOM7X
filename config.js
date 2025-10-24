2ed75b6adfa9c5f7906a1ba16eed0ae180992bd8
Kartik245206/INVESTLOOM7X <bhadana34190@gmail.com>
2025-10-24 20:49:44 +0530

    footer adding trying 3

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
     API_BASE: process.env.NODE_ENV === 'production' 
-        ? 'https://liberty-market.onrender.com'  // Update this with your actual Render URL
+        ? 'https://investloom7x.onrender.com'  // Update this with your actual Render URL
         : 'http://localhost:3000'
 };
