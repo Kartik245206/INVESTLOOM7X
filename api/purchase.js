f1686747f9b7a83ec8dc43544f53691b9c6e4c31
Kartik245206/INVESTLOOM7X <bhadana34190@gmail.com>
2025-10-28 13:47:52 +0530

    ../models/Transaction error fix into ./models/Transaction

--- api/purchase.js
+++ api/purchase.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('./auth-middleware');
-const Transaction = require('../models/Transaction');
+const Transaction = require('./models/Transaction');
 const Product = require('./models/Product');
 
 // Initiate payment
